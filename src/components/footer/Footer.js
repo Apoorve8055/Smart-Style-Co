626272295f7c7345299bad46adde9e2923cc8d73
Apoorve Verma <vapoorve@gmail.com>
2023-04-29 20:33:31 +0530

    Footer Added!

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,4 +1,47 @@
+import "./Footer.scss";
+
 const Footer = () => {
-  return <h1>Footer</h1>;
+  return (
+    <footer className="footer">
+      <div className="footer-important-links">
+        <div className="footer-subscribe">
+          <input type="text" className="input" placeholder="Enter your email" />
+          <div className="btn">Subscribe</div>
+        </div>
+        <div className="footer-list">
+          <div className="info">
+            <ul>
+              <li>Need Help?</li>
+              <li>Campaigns</li>
+              <li>Guarantee</li>
+              <li>Support</li>
+              <li>Warranty</li>
+            </ul>
+          </div>
+          <div className="social">
+            <ul>
+              <li>Facebook</li>
+              <li>LinkedIn</li>
+              <li>Instagram</li>
+              <li>YouTube</li>
+              <li>Telegram</li>
+            </ul>
+          </div>
+          <div className="useful">
+            <ul>
+              <li>Facebook</li>
+              <li>LinkedIn</li>
+              <li>Instagram</li>
+              <li>YouTube</li>
+              <li>Telegram</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="footer-copyright">
+        ©2023, Smart Style Co. | Terms & Conditions and Privacy Policy | Sitemap
+      </div>
+    </footer>
+  );
 };
 export default Footer;
