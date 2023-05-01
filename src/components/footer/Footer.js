3cb9af2348e91aa8f4ac3f3aa406ee5fb0440002
Apoorve Verma <vapoorve@gmail.com>
2023-05-01 16:44:13 +0530

    Footer Updated

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,47 +1,54 @@
 import "./Footer.scss";
-
+import Fade from "react-reveal/Fade";
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer-important-links">
-        <div className="footer-subscribe">
-          <input type="text" className="input" placeholder="Enter your email" />
-          <div className="btn">Subscribe</div>
+    <Fade>
+      <footer className="footer">
+        <div className="footer-important-links">
+          <div className="footer-subscribe">
+            <input
+              type="text"
+              className="input"
+              placeholder="Enter your email"
+            />
+            <div className="btn">Subscribe</div>
+          </div>
+          <div className="footer-list">
+            <div className="info">
+              <ul>
+                <li>Need Help?</li>
+                <li>Campaigns</li>
+                <li>Guarantee</li>
+                <li>Support</li>
+                <li>Warranty</li>
+              </ul>
+            </div>
+            <div className="social">
+              <ul>
+                <li>Facebook</li>
+                <li>LinkedIn</li>
+                <li>Instagram</li>
+                <li>YouTube</li>
+                <li>Telegram</li>
+              </ul>
+            </div>
+            <div className="useful">
+              <ul>
+                <li>Facebook</li>
+                <li>LinkedIn</li>
+                <li>Instagram</li>
+                <li>YouTube</li>
+                <li>Telegram</li>
+              </ul>
+            </div>
+          </div>
         </div>
-        <div className="footer-list">
-          <div className="info">
-            <ul>
-              <li>Need Help?</li>
-              <li>Campaigns</li>
-              <li>Guarantee</li>
-              <li>Support</li>
-              <li>Warranty</li>
-            </ul>
-          </div>
-          <div className="social">
-            <ul>
-              <li>Facebook</li>
-              <li>LinkedIn</li>
-              <li>Instagram</li>
-              <li>YouTube</li>
-              <li>Telegram</li>
-            </ul>
-          </div>
-          <div className="useful">
-            <ul>
-              <li>Facebook</li>
-              <li>LinkedIn</li>
-              <li>Instagram</li>
-              <li>YouTube</li>
-              <li>Telegram</li>
-            </ul>
-          </div>
+        <div className="footer-copyright">
+          ©2023, Smart Style Co. | Terms & Conditions and Privacy Policy |
+          Sitemap
         </div>
-      </div>
-      <div className="footer-copyright">
-        ©2023, Smart Style Co. | Terms & Conditions and Privacy Policy | Sitemap
-      </div>
-    </footer>
+      </footer>
+    </Fade>
   );
 };
 export default Footer;
